de1ba7fa1baf68048ca70786a81835882093f87f
Daniel Perez Alvarez <unindented@gmail.com>
2016-07-05 10:50:07 +0100

    Switch to eslint

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-var Remarkable = require('remarkable');
-var hljs = require('highlight.js');
+var Remarkable = require('remarkable')
+var hljs = require('highlight.js')
 
 var extend = function (obj, source) {
-  var prop;
+  var prop
 
   for (prop in source) {
     if (source.hasOwnProperty(prop)) {
-      obj[prop] = source[prop];
+      obj[prop] = source[prop]
     }
   }
 
-  return obj;
-};
+  return obj
+}
 
 module.exports = function (source) {
-  this.cacheable();
+  this.cacheable()
 
   var opts = extend({
     preset: 'default',
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
         try {
-          return hljs.highlight(lang, str).value;
+          return hljs.highlight(lang, str).value
         } catch (err) {}
       }
 
       try {
-        return hljs.highlightAuto(str).value;
+        return hljs.highlightAuto(str).value
       } catch (err) {}
 
-      return '';
+      return ''
     }
-  }, this.options.remarkable);
+  }, this.options.remarkable)
 
-  var parser = new Remarkable(opts.preset, opts);
+  var parser = new Remarkable(opts.preset, opts)
 
-  return parser.render(source);
-};
+  return parser.render(source)
+}
